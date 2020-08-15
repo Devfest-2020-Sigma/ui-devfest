5159fb1e8acf4e98296ab9a97970f3fee7e5327d
fpineau-sigma <f2pineau@sigma.fr>
2020-08-15 21:21:57 +0100

    Ajout de la route par défaut

--- front-devfest/src/app/app-routing.module.ts
+++ front-devfest/src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { visualisationRoutes } from './visualisation/visualisation.route';
 
-const routes: Routes = [...visualisationRoutes];
+const routes: Routes = [{
+  path: '',
+  redirectTo: "/visualisation/prise-photo",
+  pathMatch: 'full'
+}, ...visualisationRoutes];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
