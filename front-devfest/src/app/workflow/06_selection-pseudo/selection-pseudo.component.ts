7655c08753f09c4c71c44ea25c88a8785152f1be
HOUËT Tanguy <thouet@sigma.fr>
2021-08-11 17:06:18 +0200

    Add virtual keyboard

--- front-devfest/src/app/workflow/06_selection-pseudo/selection-pseudo.component.ts
+++ front-devfest/src/app/workflow/06_selection-pseudo/selection-pseudo.component.ts
@@ -1,47 +1,213 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { tap } from 'rxjs/operators';
-import { Image } from 'src/app/core/model/image.model';
-import { ImagesService } from '../../core/service/images.service';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
+import {Image} from 'src/app/core/model/image.model';
+import {ImagesService} from '@service/images.service';
+import Keyboard from 'simple-keyboard';
+import {environment} from '../../../environments/environment';
 
 @Component({
-  selector: 'app-selection-pseudo',
-  templateUrl: './selection-pseudo.component.html'
+    selector: 'app-selection-pseudo',
+    templateUrl: './selection-pseudo.component.html'
 })
-export class SelectionPseudoComponent implements OnInit {
+export class SelectionPseudoComponent implements OnInit, AfterViewInit
+{
 
-  public form: FormGroup;
-  public pseudo: string = "";
-  private id: string;
+    keyboard: Keyboard;
+    private lockActivated = false;
 
-  constructor(private formBuilder: FormBuilder,
-    private imagesService: ImagesService,
-    private route: ActivatedRoute,
-    private router: Router) {
-    this.form = this.formBuilder.group({
-      pseudoCtrl: ['', Validators.maxLength(10)]
-    });
-  }
+    public form: FormGroup;
+    public pseudo = '';
+    private id: string;
 
-  ngOnInit() {
-    this.form.get('pseudoCtrl').valueChanges
-      .subscribe(val => {
-        this.pseudo = val;
-      });
-    this.route.params.subscribe((params) => {
-      if (params.id) {
-        this.id = params.id;
-      }
-    });
-  }
+    constructor(private formBuilder: FormBuilder,
+                private imagesService: ImagesService,
+                private route: ActivatedRoute,
+                private router: Router)
+    {
+        this.initialiserControls();
+    }
 
-  validerPseudo() {
-    let image = new Image;
-    image._id = this.id;
-    image.pseudo = this.pseudo;
-    this.imagesService.genererSVG(image).pipe(
-      tap(() => this.router.navigate(["visualisation/impression-photo"]))
-    ).subscribe();
-  }
+    ngOnInit()
+    {
+        this.form.get('pseudoCtrl').valueChanges
+            .subscribe(val =>
+            {
+                this.pseudo = val;
+            });
+        this.route.params.subscribe((params) =>
+        {
+            if (params.id)
+            {
+                this.id = params.id;
+            }
+        });
+    }
+
+    ngAfterViewInit()
+    {
+        this.keyboard = new Keyboard({
+            layoutName: 'sigma-no',
+            theme: 'keyboard-blur hg-theme-default hg-layout',
+            layout: {
+                sigma: [
+                    '{bksp} a z e r t y u i o p',
+                    '{lock} q s d f g h j k l m',
+                    '{shift} w x c v b n , . ;',
+                    '{space} {enter}'
+                ],
+                'sigma-shift': [
+                    '{bksp} A Z E R T Y U I O P',
+                    '{lock} Q S D F G H J K L M',
+                    '{shift} W X C V B N , . ;',
+                    '{space} {enter}'
+                ],
+                'sigma-no': [
+                    '{bksp} a z e r t y u i o p',
+                    '{lock} q s d f g h j k l m',
+                    '{shift} w x c v b n , . ;',
+                    '{space}'
+                ],
+                'sigma-shift-no': [
+                    '{bksp} A Z E R T Y U I O P',
+                    '{lock} Q S D F G H J K L M',
+                    '{shift} W X C V B N , . ;',
+                    '{space}'
+                ]
+            },
+            display: {
+                '{bksp}': 'Effacer',
+                '{enter}': 'Valider',
+                '{space}': 'Espace',
+                '{shift}': 'Maj.',
+                '{lock}': 'VERR.',
+            },
+            useButtonTag: false,
+            buttonTheme: [
+                {
+                    class: 'btn btn-danger',
+                    buttons: '{bksp}'
+                },
+                {
+                    class: 'btn btn-success',
+                    buttons: '{enter}'
+                },
+            ],
+            debug: !(environment.production),
+            onChange: input => this.onChange(input),
+            onKeyPress: button => this.onKeyPress(button),
+            onKeyReleased: button => this.onKeyReleased(button),
+        });
+    }
+
+    private initialiserControls()
+    {
+        this.form = this.formBuilder.group({
+            pseudoCtrl: [this.pseudo, Validators.maxLength(10)]
+        });
+    }
+
+    validerPseudo()
+    {
+        if (this.form.touched && !this.form.dirty && this.form.valid)
+        {
+            const image = new Image();
+            image._id = this.id;
+            image.pseudo = this.pseudo;
+            this.imagesService.genererSVG(image).pipe(
+                tap(() => this.router.navigate(['visualisation/impression-photo']))
+            ).subscribe();
+        }
+    }
+
+    onChange = (input: string) =>
+    {
+        this.form.controls.pseudoCtrl.markAsTouched();
+        this.pseudo = input;
+        this.form.controls.pseudoCtrl.setValue(input);
+    }
+
+    onKeyReleased = (button: string) =>
+    {
+        if (!this.form.touched || this.form.dirty || this.form.invalid)
+        {
+            const currentLayout = this.keyboard.options.layoutName;
+            const shiftToggle = currentLayout === 'sigma-shift' || currentLayout === 'sigma-shift-no' ? 'sigma-shift-no' : 'sigma-no';
+            this.keyboard.setOptions({
+                layoutName: shiftToggle
+            });
+        }
+        else
+        {
+            const currentLayout = this.keyboard.options.layoutName;
+            const shiftToggle = currentLayout === 'sigma-shift' || currentLayout === 'sigma-shift-no' ? 'sigma-shift' : 'sigma';
+            this.keyboard.setOptions({
+                layoutName: shiftToggle
+            });
+        }
+    }
+
+    onKeyPress = (button: string) =>
+    {
+        /*
+         * If you want to handle the shift and caps lock buttons
+         */
+        if (button === '{shift}' || button === '{lock}')
+        {
+            this.lockActivated = button === '{lock}';
+            this.handleShift();
+        }
+        /*
+         * Simulation du bouton
+         */
+        else if (button === '{enter}')
+        {
+            this.validerPseudo();
+        }
+        else
+        {
+            const currentLayout = this.keyboard.options.layoutName;
+            if (!this.lockActivated && (currentLayout === 'sigma-shift' || currentLayout === 'sigma-shift-no'))
+            {
+                this.handleShiftReverse();
+            }
+        }
+    }
+
+    handleShift = () =>
+    {
+        const currentLayout = this.keyboard.options.layoutName;
+        let shiftToggle = currentLayout === 'sigma' ? 'sigma-shift' : 'sigma';
+        if (currentLayout === 'sigma-no')
+        {
+            shiftToggle = 'sigma-shift-no';
+        }
+        else if (currentLayout === 'sigma-shift-no')
+        {
+            shiftToggle = 'sigma-no';
+        }
+
+        this.keyboard.setOptions({
+            layoutName: shiftToggle
+        });
+    }
+
+    handleShiftReverse = () =>
+    {
+        const currentLayout = this.keyboard.options.layoutName;
+        let shiftToggle = currentLayout === 'sigma-shift' ? 'sigma' : 'sigma-shift';
+        if (currentLayout === 'sigma-shift-no')
+        {
+            shiftToggle = 'sigma-no';
+        }
+        else if (currentLayout === 'sigma-no')
+        {
+            shiftToggle = 'sigma-shift-no';
+        }
+
+        this.keyboard.setOptions({
+            layoutName: shiftToggle
+        });
+    }
 }
