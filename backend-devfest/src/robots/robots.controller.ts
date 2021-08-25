d4541003d4e3d90cd9c860a2e8d5c35f57dc0023
thouet-sigma <88386806+thouet-sigma@users.noreply.github.com>
2021-08-25 16:07:51 +0200

    Add robot cmd return to zero

--- backend-devfest/src/robots/robots.controller.ts
+++ backend-devfest/src/robots/robots.controller.ts
@@ -1,20 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
-import { EventPattern } from '@nestjs/microservices';
-import { processEnum } from 'src/process/process.enum';
-import { ProcessService } from 'src/process/process.service';
-import { RobotCommandEnum } from './robot.command.enum';
-import { IRobot } from './robot.interface';
-import { RobotRabbit } from './robot.rabbit';
-import { RobotsService } from './robots.service';
+import {Controller, Get} from '@nestjs/common';
+import {Param} from '@nestjs/common/decorators/http/route-params.decorator';
+import {EventPattern} from '@nestjs/microservices';
+import {processEnum} from 'src/process/process.enum';
+import {ProcessService} from 'src/process/process.service';
+import {RobotCommandEnum} from './robot.command.enum';
+import {IRobot} from './robot.interface';
+import {RobotRabbit} from './robot.rabbit';
+import {RobotsService} from './robots.service';
 
 @Controller('api/robots')
-export class RobotsController {
+export class RobotsController
+{
 
   constructor(
-    private readonly processService: ProcessService,
-    private readonly robotsService: RobotsService
-  ) { }
+      private readonly processService: ProcessService,
+      private readonly robotsService: RobotsService
+  )
+  {
+  }
 
   @Get()
   async recupererListeRobots(): Promise<IRobot[]> {
@@ -32,18 +35,36 @@ export class RobotsController {
   }
 
   @Get('/rejouer/:ip')
-  async rejouerImpressionRobot(@Param('ip') ip: string) {
-    return this.processService.execCommand(processEnum.ROBOT_CONTROLLER, ip, RobotCommandEnum.RESUME.join(" ")).catch(error => { console.log('caught', error.message); });
+  async rejouerImpressionRobot(@Param('ip') ip: string)
+  {
+    return this.processService.execCommand(processEnum.ROBOT_CONTROLLER, ip, RobotCommandEnum.RESUME.join(" ")).catch(error =>
+    {
+      console.log('caught', error.message);
+    });
   }
 
   @Get('/statut/:ip')
-  async recupererEtatRobot(@Param('ip') ip: string) {
-    return this.processService.execCommand(processEnum.ROBOT_CONTROLLER, ip, RobotCommandEnum.STATUT.join(" ")).catch(error => { console.log('caught', error.message); });
+  async recupererEtatRobot(@Param('ip') ip: string)
+  {
+    return this.processService.execCommand(processEnum.ROBOT_CONTROLLER, ip, RobotCommandEnum.STATUT.join(" ")).catch(error =>
+    {
+      console.log('caught', error.message);
+    });
+  }
+
+  @Get('/returnToZero/:ip')
+  async returnToZero(@Param('ip') ip: string)
+  {
+    return this.processService.execCommand(processEnum.ROBOT_CONTROLLER, ip, RobotCommandEnum.RETURN2ZERO.join(" ")).catch(error =>
+    {
+      console.log('caught', error.message);
+    });
   }
 
   // Integration des nouveaux robots
   @EventPattern('integration-robot')
-  async handleIntegrationRobot(data: Record<string, RobotRabbit>) {
+  async handleIntegrationRobot(data: Record<string, RobotRabbit>)
+  {
     this.robotsService.ajoutNouveauRobot(data.message.ip);
   }
 }
