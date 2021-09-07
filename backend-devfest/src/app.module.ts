9b3eb49acd70d21c82c596ab4e03b33bc950970b
thouet-sigma <88386806+thouet-sigma@users.noreply.github.com>
2021-09-07 17:48:03 +0200

    Externalisation config

--- backend-devfest/src/app.module.ts
+++ backend-devfest/src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
-import { ImagesModule } from './images/images.module';
-import { RobotsModule } from './robots/robots.module';
+import {Module} from '@nestjs/common';
+import {ImagesModule} from './images/images.module';
+import {RobotsModule} from './robots/robots.module';
+import {ConfigModule} from '@nestjs/config';
 
 
 @Module({
-  imports: [ImagesModule,
-    RobotsModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        ImagesModule,
+        RobotsModule,
+    ],
+    controllers: [],
+    providers: [],
 })
-export class AppModule { }
+export class AppModule
+{
+}
